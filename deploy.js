eb01bcf24e48dd20a10b6710a3c73b0bcf21ac1f
Frederick Wells <pandabrand@gmail.com>
2022-11-08 20:46:21 -0600

    new site up and running

---
+++ deploy.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const FtpDeploy = require('ftp-deploy')
+const ftpDeploy = new FtpDeploy()
+
+const config = {
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASS,
+    host: process.env.FTP_HOST,
+    port: process.env.FTP_PORT,
+    localRoot: __dirname,
+    remoteRoot: process.env.FTP_PATH,
+    include: ['*', '**/*'],
+    exclude: [
+        '.budfiles/**',
+        '.vscode/**',
+        'node_modules/**',
+        'node_modules/**/.*/**',
+        'bud.config.mjs',
+        '.editorconfig',
+        '.env',
+        '.git/**',
+        '.gitignore',
+        'deploy.js',
+        'yarn.lock',
+        'composer.lock',
+        'vendor/squizlabs/**',
+    ],
+    deleteRemote:false,
+    forcePasv: true,
+    sftp: true,
+}
+
+ftpDeploy
+    .deploy(config)
+    .then((res) => console.log('finished: ', res))
+    .catch((err) => console.error(err))
+
+ftpDeploy.on('uploading', (data) => {
+    console.log(data.totalFilesCount)
+    console.log(data.transferredFileCount)
+    console.log(data.filename)
+})
